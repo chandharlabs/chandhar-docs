5c3fddb696af2ebf6cebe8b78e78ef22f2154844
Pattabiram1 <rampattabhi091@gmail.com.com>
2026-01-12 11:44:29 +0530

    fullscreen

--- docs/javascripts/fullscreen.js
+++ docs/javascripts/fullscreen.js
@@ -1,39 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-  document.querySelectorAll('img[alt="Demo"]').forEach(img => {
-    img.addEventListener("click", function () {
-      const isOpen = this.classList.contains("fullscreen");
-
-      // Close any open fullscreen image
-      document.querySelectorAll("img.fullscreen").forEach(i => {
-        i.classList.remove("fullscreen");
-      });
-      document.body.classList.remove("fullscreen-open");
-
-      // Toggle this one
-      if (!isOpen) {
-        this.classList.add("fullscreen");
-        document.body.classList.add("fullscreen-open");
-      }
-    });
-  });
-});
-document.addEventListener("keydown", function (e) {
-  if (e.key === "Escape") {
-    document.querySelectorAll("img.fullscreen").forEach(i => {
-      i.classList.remove("fullscreen");
-    });
-    document.body.classList.remove("fullscreen-open");
-  }
-});
-
-document.addEventListener("DOMContentLoaded", function () {
   const images = document.querySelectorAll('img[alt="Demo"]');
 
   images.forEach(img => {
     img.addEventListener("click", function () {
       const isOpen = this.classList.contains("demo-fullscreen");
 
-      // Close any open image
+      // Close any open fullscreen image
       document.querySelectorAll("img.demo-fullscreen").forEach(i => {
         i.classList.remove("demo-fullscreen");
       });
